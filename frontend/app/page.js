877fab74ed78388f8cbbf0229b98a0bda0d7a72c
DenisMec9 <denisfilho2@gmail.com>
2025-02-14 00:19:25 +0000

    teste

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return <h1>Olá mundo</h1>; {
-  return <P>sagfdui saoidfv aboifnsqapoinfsqnaoirfnbVAYWUVFQISOPq</P>;
+  return <p>sagfdui saoidfv aboifnsqapoinfsqnaoirfnbVAYWUVFQISOPq</p>;
   }
 }

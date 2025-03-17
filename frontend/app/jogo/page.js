070f7c41418afe8bd7a08195dbed2f5c148fb0b7
Denis <denisfilho2@gmail.com>
2025-03-17 21:49:23 +0000

    Reniciamento do jogo

--- frontend/app/jogo/page.js
+++ frontend/app/jogo/page.js
@@ -1,24 +1,18 @@
-"use client"
 import React, { useState } from 'react';
-import Rodada from './com/Rodada';
-import Placar from './com/Placa';
+import Rodada from './Rodada';
+import Placar from './Placar';
 
 function Jogo() {
-  const [rodadaAtual, setRodadaAtual] = useState(1);
   const [placarJogador1, setPlacarJogador1] = useState(0);
   const [placarJogador2, setPlacarJogador2] = useState(0);
   const [vencedor, setVencedor] = useState(null);
+  const [rodadaAtual, setRodadaAtual] = useState(1);
 
-  const handleRodadaCompleta = (valorDado1, valorDado2) => {
-    if (valorDado1 > valorDado2) {
-      setPlacarJogador1(placarJogador1 + 1);
-    } else if (valorDado2 > valorDado1) {
-      setPlacarJogador2(placarJogador2 + 1);
-    }
+  const handleRodadaCompleta = (pontosJogador1, pontosJogador2) => {
+    setPlacarJogador1(placarJogador1 + pontosJogador1);
+    setPlacarJogador2(placarJogador2 + pontosJogador2);
 
-    if (rodadaAtual < 5) {
-      setRodadaAtual(rodadaAtual + 1);
-    } else {
+    if (rodadaAtual === 5) {
       if (placarJogador1 > placarJogador2) {
         setVencedor('Jogador 1');
       } else if (placarJogador2 > placarJogador1) {
@@ -29,18 +23,34 @@ function Jogo() {
     }
   };
 
+  const jogarNovamente = () => {
+    setPlacarJogador1(0);
+    setPlacarJogador2(0);
+    setVencedor(null);
+    setRodadaAtual(1);
+  };
+
   return (
     <div>
       <h1>Jogo de Dados</h1>
-      {rodadaAtual <= 5 && (
-        <Rodada
-          jogador1="Jogador 1"
-          jogador2="Jogador 2"
-          onRodadaCompleta={handleRodadaCompleta}
-        />
-      )}
+      <Rodada
+        jogador1="Jogador 1"
+        jogador2="Jogador 2"
+        onRodadaCompleta={handleRodadaCompleta}
+      />
       <Placar placarJogador1={placarJogador1} placarJogador2={placarJogador2} />
-      {vencedor && <h2>Vencedor: {vencedor}</h2>}
+      {vencedor && (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <h2 style={{ fontSize: '2em' }}>Vencedor: {vencedor}</h2>
+          <button onClick={jogarNovamente}>Jogar Novamente</button>
+        </div>
+      )}
+      {!vencedor && rodadaAtual > 5 && (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <h2>Fim de Jogo!</h2>
+          <button onClick={jogarNovamente}>Jogar Novamente</button>
+        </div>
+      )}
     </div>
   );
 }

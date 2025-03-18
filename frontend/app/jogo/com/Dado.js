59d7a5f6c72d05fe15d4b75c7ca4e576ec909451
Denis <denisfilho2@gmail.com>
2025-03-18 02:23:51 +0000

    OK

--- frontend/app/jogo/com/Dado.js
+++ frontend/app/jogo/com/Dado.js
@@ -1,19 +1,18 @@
-import React from "react";
-import dado1 from "dado1.png";
-import dado2 from "dado2.png";
-import dado3 from "dado3.png";
-import dado4 from "dado4.png";
-import dado5 from "dado5.png";
-import dado6 from "dado6.png";
+import React from 'react';
 
-const Dado = ({ valor }) => {
-  const imagens = { 1: dado1, 2: dado2, 3: dado3, 4: dado4, 5: dado5, 6: dado6 };
-
-  return (
-    <div>
-      <img src={imagens[valor]} alt={`Dado ${valor}`} width={100} height={100} />
-    </div>
-  );
+function Dado({ valor }) {
+  const imagens = {
+    1: "1.jpeg",
+    2: "2.jpeg",
+    3: "3.jpeg",
+    4: "4.jpeg",
+    5: "5.jpeg",
+    6: "6.jpeg",
 };
+
+return (
+  <img src={imagens[valor - 1]} alt={`Dado ${valor}`} style={{ width: '100px', height: '100px' }} />
+);
+}
 
 export default Dado;

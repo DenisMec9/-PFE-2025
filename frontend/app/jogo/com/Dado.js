2718c1e33763027cc61d4a50659479fee666b51f
Denis <denisfilho2@gmail.com>
2025-03-18 02:17:22 +0000

    ok

--- frontend/app/jogo/com/Dado.js
+++ frontend/app/jogo/com/Dado.js
@@ -1,10 +1,10 @@
 import React from "react";
-import dado1 from "../assets/dados/dado1.png";
-import dado2 from "../assets/dados/dado2.png";
-import dado3 from "../assets/dados/dado3.png";
-import dado4 from "../assets/dados/dado4.png";
-import dado5 from "../assets/dados/dado5.png";
-import dado6 from "../assets/dados/dado6.png";
+import dado1 from "dado1.png";
+import dado2 from "dado2.png";
+import dado3 from "dado3.png";
+import dado4 from "dado4.png";
+import dado5 from "dado5.png";
+import dado6 from "dado6.png";
 
 const Dado = ({ valor }) => {
   const imagens = { 1: dado1, 2: dado2, 3: dado3, 4: dado4, 5: dado5, 6: dado6 };

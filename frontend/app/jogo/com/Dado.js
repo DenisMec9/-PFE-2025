b51c4f59ccb18fc719c27b3abd8535a3d39bacc1
Denis <denisfilho2@gmail.com>
2025-03-18 03:03:57 +0000

    Teste

--- frontend/app/jogo/com/Dado.js
+++ frontend/app/jogo/com/Dado.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Dado({ valor }) {
+  const [valorExibido, setValorExibido] = useState(6); // Estado inicial: 6
+
+  useEffect(() => {
+    // Após um curto período, atualiza o valor exibido para o valor real
+    const timeoutId = setTimeout(() => {
+      setValorExibido(valor);
+    }, 1000); // 1000 milissegundos (1 segundo)
+
+    // Limpa o timeout se o componente for desmontado ou o valor mudar
+    return () => clearTimeout(timeoutId);
+  }, [valor]);
+
   const imagens = {
     1: "1.jpeg",
     2: "2.jpeg",
@@ -8,11 +20,15 @@ function Dado({ valor }) {
     4: "4.jpeg",
     5: "5.jpeg",
     6: "6.jpeg",
-};
+  };
 
-return (
-  <img src={imagens[valor - 1]} alt={`Dado ${valor}`} style={{ width: '100px', height: '100px' }} />
-);
+  return (
+    <img
+      src={imagens[valorExibido - 1]}
+      alt={`Dado ${valorExibido}`}
+      style={{ width: '100px', height: '100px' }}
+    />
+  );
 }
 
 export default Dado;
